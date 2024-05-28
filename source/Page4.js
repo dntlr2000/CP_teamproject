cda4e4ba887de761fe83a9937d8ea340258bfde0
dntlr2000 <60862907+dntlr2000@users.noreply.github.com>
2024-05-28 09:43:11 +0900

    제한 시간 기능 추가

--- source/Page4.js
+++ source/Page4.js
@@ -1,14 +1,15 @@
 import { Text, View, StyleSheet } from 'react-native';
 //import { useState } from 'react';
-import { Button, Answer, Wrong, Input } from './myButton';
+import { Input, TimeLimit } from './myButton';
 
 export const Page4_1 = ({ setPage, stock, setStock }) => {
     const page = 1;
     return (
         <View>
             <Text style={{ fontSize: 30, margin: 10}}>
-            비밀번호를 입력해라! 
+             비밀번호를 입력해라! 
         </Text>
+        <TimeLimit time={9} stock = {stock} setStock={setStock}/>
         </View>
     );
 };
