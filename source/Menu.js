fb6e4a70945b37276c9253552c08107da5b0580a
dntlr2000 <60862907+dntlr2000@users.noreply.github.com>
2024-05-30 17:32:55 +0900

    시작화면, 게임오버 화면 추가

---
+++ source/Menu.js
@@ -0,0 +1,38 @@
+import { Text, View, StyleSheet } from 'react-native';
+//import { useState } from 'react';
+import { Button } from './myButton';
+
+export const Menu_1 = ({ setPage, stock, setStock }) => {
+    const page = 0;
+    return (
+        <View>
+            <Text style={{ fontSize: 40, margin: 10, font:'bold'}}>
+            센스 퀴즈
+        </Text>
+        <Text style={{ fontSIze: 30, margin: 10}}>크로스플랫폼 7조</Text>
+        </View>
+    );
+};
+
+export const Menu_2 = ({ setPage, setStock }) => {
+    const page = 0;
+    return (
+        <View>
+            <Answer setPage={setPage} setStock={setStock}>게임 시작</Answer>
+        </View>
+    );
+};
+
+export const Answer = (props) => {
+    const BasicText = 'Answer Button'
+    const startGame = () => {
+        props.setPage(1)
+        props.setStock(3)
+    }
+    return <View>
+        <Button onPress={startGame}
+        style = {props.style} 
+        textStyle = {props.textStyle}>
+            {props.children||BasicText}</Button>
+    </View>
+}
