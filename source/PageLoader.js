d34e18aa84280977ea2b3f7dfb1fc3801d13f4f7
dntlr2000 <60862907+dntlr2000@users.noreply.github.com>
2024-05-30 17:33:46 +0900

    메뉴, 게임오버 화면 구현

--- source/PageLoader.js
+++ source/PageLoader.js
@@ -1,6 +1,8 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { useState } from 'react';
-import { Button, Answer, Wrong } from './myButton';
+import { Button, Answer, Wrong, FinalAnswer } from './myButton';
+
+import { Menu_1, Menu_2 } from './Menu';
 
 import {Page1_1, Page1_2} from './Page1'
 import {Page2_1, Page2_2} from './Page2'
@@ -10,39 +12,52 @@ import {Page5_1, Page5_2} from './Page5'
 import {Page6_1, Page6_2} from './Page6'
 import {Page7_1, Page7_2} from './Page7'
 
+import {Final_1, Final_2} from './FinalPage'
+import {Over_1, Over_2} from './GameOver'
+
 const TopPage = ({ page, setPage, stock, setStock }) => {
+    
     return <View>
-            {page === 1 && <Page1_1/>}
-            {page === 2 && <Page2_1/>}
-            {page === 3 && <Page3_1/>}
-            {page === 4 && <Page4_1 stock = {stock} setStock = {setStock}/>}
-            {page === 5 && <Page5_1 setPage = {setPage} stock = {stock} setStock = {setStock}/>}
-            {page === 6 && <Page6_1/>}
-            {page === 7 && <Page7_1/>}
-        <Text style={{ fontSize: 30, margin: 10}}>
-            {page > 7 && '마지막 문제'}
-        </Text>
-        <Text style={{ fontSize: 25, padding: 10 }}>목숨: {stock}</Text>
+            {page === 0 && <Menu_1/>}
+            {stock === 0 && <Over_1 setStock={setStock}/>}
+            {stock > 0 && <>
+                {page === 1 && <Page1_1/>}
+                {page === 2 && <Page2_1/>}
+                {page === 3 && <Page3_1/>}
+                {page === 4 && <Page4_1 stock = {stock} setStock = {setStock}/>}
+                {page === 5 && <Page5_1 setPage = {setPage} stock = {stock} setStock = {setStock}/>}
+                {page === 6 && <Page6_1/>}
+                {page === 7 && <Page7_1/>}
+                {page > 7 && <Final_1/>}
+
+                <Text style={{ fontSize: 25, padding: 10 }}>목숨: {stock}</Text>
+            </>}
+        
+        
     </View>
 }
 
 const LowerPage = ({page, setPage, stock, setStock}) => {
     return <View>
-            {page === 1 && <Page1_2 setPage={setPage} stock={stock} setStock={setStock} />}
-            {page === 2 && <Page2_2 setPage={setPage} stock={stock} setStock={setStock} />}
-            {page === 3 && <Page3_2 setPage={setPage} stock={stock} setStock={setStock} />}
-            {page === 4 && <Page4_2 setPage={setPage} stock={stock} setStock={setStock} />}
-            {page === 5 && <Page5_2 setPage={setPage} stock={stock} setStock={setStock} />}
-            {page === 6 && <Page6_2 setPage={setPage} stock={stock} setStock={setStock} />}
-            {page === 7 && <Page7_2 setPage={setPage} stock={stock} setStock={setStock} />}
-            {page > 7 && <PageEnd setPage={setPage} stock={stock} setStock={setStock} />}
+            {page === 0 && <Menu_2 setPage={setPage} setStock={setStock} />}
+            {stock === 0 && <Over_2 setPage={setPage} page = {page} setStock = {setStock}/>}
+            {stock > 0 && <>
+                {page === 1 && <Page1_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 2 && <Page2_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 3 && <Page3_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 4 && <Page4_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 5 && <Page5_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 6 && <Page6_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 7 && <Page7_2 setPage={setPage} stock={stock} setStock={setStock} />}
+
+                {page > 7 && <Final_2 setPage={setPage} stock={stock} setStock={setStock} />}
+            </>}
     </View>
 }
 
 export default function Page() {
-    const [stock, setStock] = useState(3);
-    const [page, setPage] = useState(1);
-
+    const [stock, setStock] = useState(-1);
+    const [page, setPage] = useState(0);
     return (
         <View style={styles.container}>
             <View style={styles.topContainer}>
@@ -55,15 +70,6 @@ export default function Page() {
     );
 }
 
-const PageEnd = ({ setPage, stock, setStock }) => {
-    const page = 3;
-    return (
-        <View>
-            <Text style={{ width: 500, height: 50 }}> End of Page </Text>
-            <Wrong stock={stock} setStock={setStock}>정답아님</Wrong>
-        </View>
-    );
-};
 
 const styles = StyleSheet.create({
     container: {
