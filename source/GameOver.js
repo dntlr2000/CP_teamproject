feccbbf0e28f2a94fb4478da00782df722d13252
dntlr2000 <60862907+dntlr2000@users.noreply.github.com>
2024-06-01 14:07:18 +0900

    문제 수가 늘어남에 따라 출력 텍스트 조건 수정

--- source/GameOver.js
+++ source/GameOver.js
@@ -17,9 +17,9 @@ export const Over_2 = ({ setPage, setStock, page }) => {
         <View>
             <Text style={{ fontSize: 40, margin: 10}}>맞힌 문제: {page - 1}개</Text>
             <Text style={{ fontSize: 30, margin: 10}}>
-                {page < 4 && '에고고... 신중하게 풀어보세요!'}
-                {page >= 4 && page <= 7 && '조금만 더 노력하면 끝까지 풀 수 있어요!'}
-                {page > 7 && '끝까지 푸셨군요! 축하드려요!'}
+                {page < 5 && '에고고... 신중하게 풀어보세요!'}
+                {page >= 5 && page <= 10 && '조금만 더 노력하면 끝까지 풀 수 있어요!'}
+                {page > 10 && '끝까지 푸셨군요! 축하드려요!'}
             </Text>
             <ResetButton setPage={setPage} setStock= {setStock}>메인 화면으로</ResetButton>
         </View>
