1ea7fb486df02afa6e23c1e61248b0734117e5ce
dntlr2000 <60862907+dntlr2000@users.noreply.github.com>
2024-06-01 13:44:22 +0900

    마지막 문제 수정

--- source/FinalPage.js
+++ source/FinalPage.js
@@ -7,17 +7,17 @@ export const Final_1 = ({ setPage, stock, setStock }) => {
     return (
         <View>
             <Text style={{ fontSize: 30, margin: 10}}>
-            엔딩 보기 싫으면 절대로 누르지마
+            당신이 이 프로젝트에 등급을 매긴다면 몇 등급을 매기시겠습니까?
         </Text>
         </View>
     );
 };
 
-export const Final_2 = ({ setPage, stock, setStock }) => {
-    const page = 99;
+export const Final_2 = ({ page, setPage, stock, setStock }) => {
+
     return (
         <View>
-            <FinalAnswer setPage={setPage} setStock={setStock}>절대로! 누르지마!</FinalAnswer>
+            <FinalAnswer setPage={setPage} setStock={setStock} page={page}>A+</FinalAnswer>
         </View>
     );
 };
