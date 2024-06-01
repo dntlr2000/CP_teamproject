2709c76cc2a51f1149683cbccbf21b705d749ecf
dntlr2000 <60862907+dntlr2000@users.noreply.github.com>
2024-06-01 14:09:36 +0900

    10 + 1번까지 추가
    쓸모 없는 코드가 있긴 한데 나중에 여유 있으면 하나하나 알아보며 제거할 예정

--- source/PageLoader.js
+++ source/PageLoader.js
@@ -10,11 +10,14 @@ import {Page4_1, Page4_2} from './Page4'
 import {Page5_1, Page5_2} from './Page5'
 import {Page6_1, Page6_2} from './Page6'
 import {Page7_1, Page7_2} from './Page7'
+import {Page8_1, Page8_2} from './Page8'
+import {Page9_1, Page9_2} from './Page9'
+import {Page10_1, Page10_2} from './Page10'
 
 import {Final_1, Final_2} from './FinalPage'
 import {Over_1, Over_2} from './GameOver'
 
-const TopPage = ({ page, setPage, stock, setStock }) => {
+const TopPage = ({ page, setPage, stock, setStock, isDark, setIsDark }) => {
     
     return <View>
             {page === 0 && <Menu_1/>}
@@ -27,16 +30,19 @@ const TopPage = ({ page, setPage, stock, setStock }) => {
                 {page === 5 && <Page5_1 setPage = {setPage} stock = {stock} setStock = {setStock}/>}
                 {page === 6 && <Page6_1/>}
                 {page === 7 && <Page7_1/>}
-                {page > 7 && <Final_1/>}
+                {page === 8 && <Page8_1/>}
+                {page === 9 && <Page9_1/>}
+                {page === 10 && <Page10_1 isDark = {isDark} setPage = {setPage}/>}
+                {page > 10 && <Final_1/>}
 
-                <Text style={{ fontSize: 25, padding: 10 }}>목숨: {stock}</Text>
+                {page != 5 && <Text style={{ fontSize: 25, padding: 10 }}>목숨: {stock}</Text>}
             </>}
         
         
     </View>
 }
 
-const LowerPage = ({page, setPage, stock, setStock}) => {
+const LowerPage = ({page, setPage, stock, setStock, isDark, setIsDark}) => {
     return <View>
             {page === 0 && <Menu_2 setPage={setPage} setStock={setStock} />}
             {stock === 0 && <Over_2 setPage={setPage} page = {page} setStock = {setStock}/>}
@@ -48,8 +54,10 @@ const LowerPage = ({page, setPage, stock, setStock}) => {
                 {page === 5 && <Page5_2 setPage={setPage} stock={stock} setStock={setStock} />}
                 {page === 6 && <Page6_2 setPage={setPage} stock={stock} setStock={setStock} />}
                 {page === 7 && <Page7_2 setPage={setPage} stock={stock} setStock={setStock} />}
-
-                {page > 7 && <Final_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 8 && <Page8_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 9 && <Page9_2 setPage={setPage} stock={stock} setStock={setStock} />}
+                {page === 10 && <Page10_2 setPage={setPage} stock={stock} setStock={setStock} isDark = {isDark} setIsDark = {setIsDark} />}
+                {page > 10 && <Final_2 setPage={setPage} stock={stock} setStock={setStock} page={page}/>}
             </>}
     </View>
 }
@@ -57,13 +65,14 @@ const LowerPage = ({page, setPage, stock, setStock}) => {
 export default function Page() {
     const [stock, setStock] = useState(-1);
     const [page, setPage] = useState(0);
+    const [isDark, setIsDark] = useState(false);
     return (
         <View style={styles.container}>
             <View style={styles.topContainer}>
-                <TopPage page={page} setPage={setPage} stock={stock} setStock={setStock}/>
+                <TopPage page={page} setPage={setPage} stock={stock} setStock={setStock} isDark = {isDark} setIsDark = {setIsDark}/>
             </View>
             <View style={styles.bottomContainer}>
-                <LowerPage page = {page} setPage={setPage} stock={stock} setStock={setStock}/>
+                <LowerPage page = {page} setPage={setPage} stock={stock} setStock={setStock} isDark = {isDark} setIsDark = {setIsDark}/>
             </View>
         </View>
     );
@@ -85,7 +94,7 @@ const styles = StyleSheet.create({
     topContainer: {
         flex: 1,
         justifyContent: 'center',
-        //backgroundColor: '#FFCCCC',
+        backgroundColor: '#FFFFFF',
         borderBottomWidth: 2,
         borderBottomColor: 'black'
     },
@@ -94,5 +103,22 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#CCCCFF',
-    }
+    },
+
+    lightTheme: {
+        backgroundColor: '#ffffff'  
+      },
+      darkTheme: {
+          backgroundColor: '#000000'
+      },
+      blackText: {
+          color: 'black',
+          fontSize: 30,
+          margin: 10
+      },
+      grayText: {
+          color: '#666666',
+          fontSize: 30,
+          margin: 10,
+      }
 });
